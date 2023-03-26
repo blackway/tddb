6a94f6a69f1ac288d76471e222c63bbe8a6b15cf
Im Dae Lim <blackway76@gmail.com>
2023-03-26 20:59:58 +0900

    Order을 사용하기 위해서 추가 테스트 메소드를 순번은 지정 해서 순서대로 실행 하도록 할수 있다.

--- chap02/src/main/java/chap02/Calculator.java
+++ chap02/src/main/java/chap02/Calculator.java
@@ -2,6 +2,7 @@ package chap02;
 
 public class Calculator {
     public static int plus(int a1, int a2) {
-        return a1 + a2;
+        return 3;
+//        return a1 + a2;
     }
 }
--- chap02/src/test/java/chap02/PasswordStrengthMeterTest.java
+++ chap02/src/test/java/chap02/PasswordStrengthMeterTest.java
@@ -1,9 +1,15 @@
 package chap02;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+
+//@Order을 사용하기 위해서 추가 테스트 메소드를 순번은 지정 해서 순서대로 실행 하도록 할수 있다.
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class PasswordStrengthMeterTest {
 
     private PasswordStrengthMeter meter = new PasswordStrengthMeter();
@@ -13,54 +19,65 @@ public class PasswordStrengthMeterTest {
         assertEquals(expStr, result);
     }
 
+    //@Order를 추가하여 사용자가 원하는 순서를 지정합니다.
     @Test
+    @Order(1)
     void meetsAllCriteria_Then_Strong() {
         assertStrength("ab12!@AB", PasswordStrength.STRONG);
         assertStrength("abc1!Add", PasswordStrength.STRONG);
     }
 
     @Test
+    @Order(2)
     void meetsOtherCriteria_except_for_Length_Then_Normal() {
         assertStrength("ab12!@A", PasswordStrength.NORMAL);
         assertStrength("Ab12!c", PasswordStrength.NORMAL);
     }
 
     @Test
+    @Order(3)
     void meetsOtherCriteria_except_for_number_Then_Normal() {
         assertStrength("ab!@ABqwer", PasswordStrength.NORMAL);
     }
 
     @Test
+    @Order(4)
     void nullInput_Then_Invalid() {
         assertStrength(null, PasswordStrength.INVALID);
     }
 
     @Test
+    @Order(5)
     void emptyInput_Then_Invalid() {
         assertStrength("", PasswordStrength.INVALID);
     }
 
     @Test
+    @Order(6)
     void meetsOtherCriteria_except_for_Uppercase_Then_Normal() {
         assertStrength("ab12!@df", PasswordStrength.NORMAL);
     }
 
     @Test
+    @Order(7)
     void meetsOnlyLengthCriteria_Then_Weak() {
         assertStrength("abdefghi", PasswordStrength.WEAK);
     }
 
     @Test
+    @Order(8)
     void meetsOnlyNumCriteria_Then_Weak() {
         assertStrength("12345", PasswordStrength.WEAK);
     }
 
     @Test
+    @Order(9)
     void meetsOnlyUpperCriteria_Then_Weak() {
         assertStrength("ABZEF", PasswordStrength.WEAK);
     }
 
     @Test
+    @Order(10)
     void meetsNoCriteria_Then_Weak() {
         assertStrength("abc", PasswordStrength.WEAK);
     }
