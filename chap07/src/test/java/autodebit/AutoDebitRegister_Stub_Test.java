724d1f00cdbc97a4553ec8919627a480868b1593
blackway76 <blackway76@gmail.com>
2023-05-14 21:49:53 +0900

    20230514 group study 준비 2.내용 추가

--- chap07/src/test/java/autodebit/AutoDebitRegister_Stub_Test.java
+++ chap07/src/test/java/autodebit/AutoDebitRegister_Stub_Test.java
@@ -23,7 +23,12 @@ public class AutoDebitRegister_Stub_Test {
 	@Test
 	void invalidCard() {
 		// 유효하지 않을 번호 설정.
-		stubValidator.setInvalidNo("111122223333");
+		if(active.equals("local")){
+
+			stubValidator.setInvalidNo("111122223333");
+		}else {
+			//진짜 카드사에서 받은 번호
+호		}
 
 		AutoDebitReq req = new AutoDebitReq("user1", "111122223333");
 		RegisterResult result = this.register.register(req);
